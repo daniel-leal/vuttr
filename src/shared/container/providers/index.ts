6e62f0bfd09cb3bb0340fa3e0ccd25d8702125d8
Daniel Leal <dleal@banparanet.com.br>
2021-01-14 15:19:14 -0300

    Init repo

---
+++ src/shared/container/providers/index.ts
@@ -0,0 +1,17 @@
+import { container } from 'tsyringe';
+
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+
+import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
+import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
+container.registerInstance<IMailTemplateProvider>(
+  'MailTemplateProvider',
+  new HandlebarsMailTemplateProvider(),
+);
+
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  container.resolve(EtherealMailProvider),
+);
